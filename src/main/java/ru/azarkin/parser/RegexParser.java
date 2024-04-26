f72a3eec1dd5b6ab9e813ec826081758a811a0c0
Vitaliy Azarkin <v.azarkin@gmail.com>
2024-04-26 23:27:16 +0300

    Add regex optimization (20% faster)

--- src/main/java/ru/azarkin/parser/RegexParser.java
+++ src/main/java/ru/azarkin/parser/RegexParser.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
  * 192.168.32.181 - - [14/06/2017:16:47:02 +1000] "PUT /rest/v1.4/documents?zone=default&_rid=6076537c HTTP/1.1" 200 2 44.510983 "-" "@list-item-updater" prio:0
  */
 public class RegexParser implements Parser {
-    private static final Pattern LOG_PATTERN = Pattern.compile("^[0-9\\.]+ - - \\[([\\d\\/: \\+]+)\\] \\\".+\\\" (\\d+) \\d+ (\\d{0,3})\\..+$", Pattern.MULTILINE);
+    private static final Pattern LOG_PATTERN = Pattern.compile("^[0-9.]+ - - \\[([\\d/: +]+)] \".+\" (\\d+) \\d+ (\\d{0,3})\\..+$");
 
     public LogItem parse(String logString) throws RuntimeException {
         Matcher matcher = LOG_PATTERN.matcher(logString);
